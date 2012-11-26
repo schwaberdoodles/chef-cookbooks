074b5574cd137c1e2793952f63a378c5ef10bc4c
Murali Raju <murali.raju@appliv.com>
2012-11-26 12:55:41 -0500

    Scaffolding

---
+++ pxe/attributes/pxe.rb
@@ -0,0 +1,1 @@
+default[:pxe][:dir] = "/etc/dhcpd"
--- pxe/recipes/default.rb
+++ pxe/recipes/default.rb
@@ -6,3 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "dhcp3-server"
+
+service "isc-dhcp-server" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
+
+template "/etc/dhcp/dhcpd.conf" do
+  notifies :reload, "service[isc-dhcp-server]"
+end
