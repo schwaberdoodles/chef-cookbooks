3757be30c6e206def162d085105e01b6d42b0f74
Murali Raju <murali.raju@appliv.com>
2012-11-26 14:31:01 -0500

    Name changes

--- pxe/attributes/pxe.rb
+++ pxe/attributes/pxe.rb
@@ -1,6 +1,6 @@
 default[:pxe][:dhcp][:dns_server] = "10.10.1.250"
-default[:pxe][:dhcp][:pxe_server] = "10.10.1.251"
+default[:pxe][:dhcp][:next_server] = "10.10.1.251"
 default[:pxe][:dhcp][:subnet] = "10.10.1.0"
 default[:pxe][:dhcp][:subnet_mask] = "255.255.255.0"
 default[:pxe][:dhcp][:host_range] = "10.10.1.150 10.10.1.200"
-default[:pxe][:dhcp][:interfaces] = "eth0"
+default[:pxe][:dhcp][:interfaces] = ['eth0']
