4c43391574cde3107d801bc8220271a521903bc5
Murali Raju <murali.raju@appliv.com>
2012-11-29 14:04:19 -0500

    Skeleton seed configs for Ubuntu

--- pxe/attributes/pxe.rb
+++ pxe/attributes/pxe.rb
@@ -1,10 +1,10 @@
-default[:pxe][:dhcpd][:dns_server] = "10.10.1.250"
-default[:pxe][:dhcpd][:next_server] = "10.10.1.251"
-default[:pxe][:dhcpd][:subnet] = "10.10.1.0"
+default[:pxe][:dhcpd][:dns_server] = "192.168.73.250"
+default[:pxe][:dhcpd][:next_server] = "192.168.73.136"
+default[:pxe][:dhcpd][:subnet] = "192.168.73.0"
 default[:pxe][:dhcpd][:subnet_mask] = "255.255.255.0"
-default[:pxe][:dhcpd][:broadcast] = "10.10.1.255"
-default[:pxe][:dhcpd][:gateway] = "10.10.1.1"
-default[:pxe][:dhcpd][:host_range] = "10.10.1.150 10.10.1.200"
+default[:pxe][:dhcpd][:broadcast] = "192.168.73.255"
+default[:pxe][:dhcpd][:gateway] = "192.168.73.1"
+default[:pxe][:dhcpd][:host_range] = "192.168.73.150 192.168.73.200"
 default[:pxe][:dhcpd][:interfaces] = ['eth0']
 default[:pxe][:dhcpd][:filename] = "pxelinux.0"
 default[:pxe][:dhcpd][:databag] = "dhcpd"
@@ -22,6 +22,6 @@ default[:pxe][:releases] = [
 
 # add list(s) for your target node(s)
 default[:pxe][:servers] = [
-  { :mac => "00:50:56:3A:76:F0", :release => "ubuntu-12.04", :ip => "192.168.73.191", :hostname => "node1" },
+  { :mac => "00:50:56:3A:76:F0", :release => "ubuntu-12.04", :ip => "192.168.73.198", :hostname => "node1" },
   { :mac => "00:1c:25:74:ef:79", :release => "debian-6.0.5", :ip => "192.168.73.190", :hostname => "node2" }
 ]
