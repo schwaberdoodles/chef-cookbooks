a633f0ba5d0accda9ef8c3cdab40ac85a91a0f4c
Murali Raju <murali.raju@appliv.com>
2012-11-30 17:31:38 -0500

    rename pxe to ucs-pxe

--- pxe/create_data_bags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `ls data_bags` ; do 
-    for j in `ls data_bags/$i/`; do
-        knife data bag create $i
-            knife data bag from file $i data_bags/$i/$j ;
-       done ;
-    done
--- pxe/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Velankani Information Systems, Inc"
-maintainer_email "eng@velankani.net"
-license          "All rights reserved"
-description      "Installs/Configures a PXE Appliance used for bare metal provisioning"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
