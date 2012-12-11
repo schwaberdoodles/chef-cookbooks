087c18a6dd86ea89eb012933248c1920609e2157
Murali Raju <murali.raju@appliv.com>
2012-12-11 09:56:32 -0500

    Minor fixes

--- bigdatadev/attributes/bigdatadev.rb
+++ bigdatadev/attributes/bigdatadev.rb
@@ -1,4 +1,4 @@
 default[:bigdatadev][:hadoop][:dist] = "cdh4"
 default[:bigdatadev][:hadoop][:path] = "http://archive.cloudera.com/cdh4/one-click-install/precise/amd64/cdh4-repository_1.0_all.deb"
 default[:bigdatadev][:hadoop][:java_home] = "/usr/lib/jvm/jdk1.6.0_37/"
-default[:bigdatadev][:hadoop][:user] = "hdpuser"
+default[:bigdatadev][:hadoop][:user] = "cdhuser"
