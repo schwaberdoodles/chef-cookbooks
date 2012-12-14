05bcb9fbb19e70f96dd2bfbfbebcc9e723eae721
Murali Raju <murali.raju@appliv.com>
2012-12-13 19:28:30 -0500

    Testing multi-chassis for server pools on initials recipe

--- ucs-solo/recipes/pools.rb
+++ ucs-solo/recipes/pools.rb
@@ -54,9 +54,7 @@ pools.each do |pool_info|
   wwnn_pool_json = {:wwnn_name => "#{pool['wwnn']['name']}", :wwnn_from => "#{pool['wwnn']['from']}",
                     :wwnn_to => "#{pool['wwnn']['to']}", :org => "#{pool['wwnn']['org']}"}.to_json
 
-  server_pool_json = {:server_pool_name => "#{pool['server']['server_pool_name']}", :server_pool_description => "#{pool['server']['server_pool_description']}",
-                      :server_pool_chassis_id => "#{pool['server']['server_pool_chassis_ids']}", :server_pool_blades => "#{pool['server']['server_pool_blades']}",
-                      :org => "#{pool['server']['org']}"}.to_json
+
 
   ucs_provision.set_uuid_pool(uuid_pool_json)
   log "Created UUID Pool #{pool['uuid']['name']} from #{pool['uuid']['from']} to #{pool['uuid']['to']} in org #{pool['uuid']['org']}"
@@ -67,9 +65,13 @@ pools.each do |pool_info|
   ucs_provision.set_wwnn_pool(wwnn_pool_json)
   log "Created WWNN Pool #{pool['wwnn']['name']} from #{pool['wwnn']['from']} to #{pool['wwnn']['to']} in org #{pool['wwnn']['org']}"
 
-
-  ucs_provision.set_server_pool(server_pool_json)
-  log "Created Server Pool #{pool['server']['server_pool_name']} on chassis #{pool['server']['server_pool_chassis_id']} using blades #{pool['server']['server_pool_blades']}"
-  
+  server_pool_chassis_ids = "#{pool['server']['server_pool_chassis_ids']}".to_s.split(',')
+  server_pool_chassis_ids.each do |server_pool_chassis_id|
+  server_pool_json = {:server_pool_name => "#{pool['server']['server_pool_name']}", :server_pool_description => "#{pool['server']['server_pool_description']}",
+                      :server_pool_chassis_id => "#{pool['server']['server_pool_chassis_id']}", :server_pool_blades => "#{pool['server']['server_pool_blades']}",
+                      :org => "#{pool['server']['org']}"}.to_json
+    ucs_provision.set_server_pool(server_pool_json)
+    log "Created Server Pool #{pool['server']['server_pool_name']} on chassis #{pool['server']['server_pool_chassis_id']} using blades #{pool['server']['server_pool_blades']}"
+  end
   
 end
