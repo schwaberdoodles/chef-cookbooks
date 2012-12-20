bc9229d8b0b82c99c0f529eb8645c1f40939b743
Murali Raju <murali.raju@appliv.com>
2012-12-19 22:24:57 -0500

    Attribute change for testing

--- metalprov/attributes/metalprov.rb
+++ metalprov/attributes/metalprov.rb
@@ -14,12 +14,12 @@ default[:metalprov][:razor][:mk_path] = "https://github.com/downloads/puppetlabs
 
 #DHCP
 default[:metalprov][:dhcpd][:dns_server] = "8.8.8.8"
-default[:metalprov][:dhcpd][:next_server] = "172.16.10.5"
-default[:metalprov][:dhcpd][:subnet] = "172.16.10.0"
+default[:metalprov][:dhcpd][:next_server] = "192.168.73.162"
+default[:metalprov][:dhcpd][:subnet] = "192.168.73.0"
 default[:metalprov][:dhcpd][:subnet_mask] = "255.255.255.0"
-default[:metalprov][:dhcpd][:broadcast] = "172.16.10.255"
-default[:metalprov][:dhcpd][:gateway] = "10.0.2.2"
-default[:metalprov][:dhcpd][:host_range] = "172.16.10.150 172.16.10.200"
-default[:metalprov][:dhcpd][:interfaces] = ['eth1']
+default[:metalprov][:dhcpd][:broadcast] = "192.168.73.255"
+default[:metalprov][:dhcpd][:gateway] = "192.168.73.2"
+default[:metalprov][:dhcpd][:host_range] = "192.168.73.150 192.168.73.200"
+default[:metalprov][:dhcpd][:interfaces] = ['eth0']
 default[:metalprov][:dhcpd][:filename] = "pxelinux.0"
 
