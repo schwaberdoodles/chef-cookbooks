027f9127001c3439591d79b1bbf236e85fa533d3
Murali Raju <murali.raju@appliv.com>
2013-04-21 20:47:18 -0400

    Fixes

--- bigdatadev/recipes/hadoop_cdh_master.rb
+++ bigdatadev/recipes/hadoop_cdh_master.rb
@@ -27,27 +27,15 @@ script "Setting up and start Cloudera Manager" do
   interpreter "bash"
   #user "root"
   code <<-EOH
-  REPOCM=${REPOCM:-cm4}
-  CM_REPO_HOST=${CM_REPO_HOST:-archive.cloudera.com}
-  CM_MAJOR_VERSION=$(echo $REPOCM | sed -e 's/cm\\([0-9]\\).*/\\1/')
-  CM_VERSION=$(echo $REPOCM | sed -e 's/cm\\([0-9][0-9]*\\)/\\1/')
-  OS_CODENAME=$(lsb_release -sc)
-  OS_DISTID=$(lsb_release -si | tr '[A-Z]' '[a-z]')
-  if [ $CM_MAJOR_VERSION -ge 4 ]; then
-    cat > /etc/apt/sources.list.d/cloudera-$REPOCM.list <<EOF
-  deb [arch=amd64] http://$CM_REPO_HOST/cm$CM_MAJOR_VERSION/$OS_DISTID/$OS_CODENAME/amd64/cm $OS_CODENAME-$REPOCM contrib
-  deb-src http://$CM_REPO_HOST/cm$CM_MAJOR_VERSION/$OS_DISTID/$OS_CODENAME/amd64/cm $OS_CODENAME-$REPOCM contrib
-  EOF
-  curl -s http://$CM_REPO_HOST/cm$CM_MAJOR_VERSION/$OS_DISTID/$OS_CODENAME/amd64/cm/archive.key > key
-  apt-key add key
+  curl -s http://archive.cloudera.com/cm4/ubuntu/precise/amd64/cm/archive.key > key
+  sudo apt-key add key
   rm key
-  fi
-  apt-get update
+  sudo apt-get update
   export DEBIAN_FRONTEND=noninteractive
-  apt-get -q -y --force-yes install oracle-j2sdk1.6 cloudera-manager-server-db cloudera-manager-server cloudera-manager-daemons
-  service cloudera-scm-server-db initdb
-  service cloudera-scm-server-db start
-  service cloudera-scm-server start
+  sudo apt-get -q -y --force-yes install oracle-j2sdk1.6 cloudera-manager-server-db cloudera-manager-server cloudera-manager-daemons
+  sudo service cloudera-scm-server-db initdb
+  sudo service cloudera-scm-server-db start
+  sudo service cloudera-scm-server start
   EOH
 end
 
