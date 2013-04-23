a1b1e6afb23cfa29c284f7d4beeaca09c1a8aa85
Murali Raju <murali.raju@appliv.com>
2013-04-22 20:36:20 -0400

    refactor

--- bigdatadev/recipes/hadoop_cdh_master.rb
+++ bigdatadev/recipes/hadoop_cdh_master.rb
@@ -20,24 +20,24 @@
 # This recipe is for Cloudera Hadoop CDH4 on Ubuntu 12.04.LTS only
 # Most of this will refactored in a later version
 
-include_recipe "java::oracle"
+#include_recipe "java::oracle"
 
 package "curl"
 
 
 script "Setting up and start Cloudera Manager" do
   interpreter "bash"
-  #user "root"
+  user "root"
   code <<-EOH
   curl -s http://archive.cloudera.com/cm4/ubuntu/precise/amd64/cm/archive.key > key
-  sudo apt-key add key
+  apt-key add key
   rm key
-  sudo apt-get update
+  apt-get update
   export DEBIAN_FRONTEND=noninteractive
-  sudo apt-get -q -y --force-yes install oracle-j2sdk1.6 cloudera-manager-server-db cloudera-manager-server cloudera-manager-daemons
-  sudo service cloudera-scm-server-db initdb
-  sudo service cloudera-scm-server-db start
-  sudo service cloudera-scm-server start
+  apt-get -q -y --force-yes install oracle-j2sdk1.6 cloudera-manager-server-db cloudera-manager-server cloudera-manager-daemons
+  service cloudera-scm-server-db initdb
+  service cloudera-scm-server-db start
+  service cloudera-scm-server start
   EOH
 end
 
